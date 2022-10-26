ba96784174d77b376e8613f78159f0914ce88d75
Killomaker <105066335+Killomaker@users.noreply.github.com>
2022-10-26 21:24:46 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,28 @@
+import pickle
+import socket 
+
+print('Server launched')
+
+sock = socket.socket()
+sock.bind(('', 9090))
+
+print('Starting. . .')
+sock.listen(1)
+
+print('Connecting. . .')
+
+connec, adr = sock.accept()
+
+print('Getting data. . .')
+
+mes = connec.recv(1024).decode()
+print(mes)
+
+
+print('Sending. . .
+connec.send(mes.encode())
+
+print('Turning off the client')
+connec.close()
+
+print('Server turned off')
